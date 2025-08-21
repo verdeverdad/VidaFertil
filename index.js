0d00142381ad21c80dd18e3ac1442c8f7a77a823
Verde <sofia.16d@gmail.com>
2025-08-20 23:33:22 -0300

    footer y navbar

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed('#element', {
         strings: ['<i>Más de treinta años<br> trabajando por una vida<br> más sustentable</i>', '<i>y hoy<br> tenemos la misma ilusión <br>que el primer día.</i>'],
-        typeSpeed: 50,
+        typeSpeed: 30,
         loop: true,
         backDelay: 900,
-        backSpeed: 10,
+        backSpeed: 20,
 
     });
 });
